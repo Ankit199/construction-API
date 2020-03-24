a6b59639571827eb0b922fef1f46ccd4358c55cb
Ankit199 <ankitshukla1992@outlook.com>
2020-03-24 02:59:34 -0400

    code implemented for inout

--- controller/InOutController.js
+++ controller/InOutController.js
@@ -5,114 +5,104 @@ var conn = require("../connectionDB/dbconfig")();
 var jwt = require("jsonwebtoken");
 var config = require("../config");
 
-var InOutRoute = function () {
+var InOutRoute = function() {
+  /**
+   * ! ? Method Implemented for Add Item in stock
+   */
 
-/**
- * ! ? Method Implemented for Add Item in stock 
- */
+  router.route("/additem").post(function(req, res) {
+    var token = req.headers["x-access-token"];
 
+    if (!token)
+      return res
+        .status(401)
+        .send({ auth: false, message: "No token provided." });
 
-    router.route("/additem").post(function (req, res) {
-        var token = req.headers["x-access-token"];
+    jwt.verify(token, config.secret, function(err, decoded) {
+      if (err)
+        return res
+          .status(500)
+          .send({ auth: false, data: "Failed to authenticate token." });
 
-        if (!token)
-            return res
-                .status(401)
-                .send({ auth: false, message: "No token provided." });
-
-        jwt.verify(token, config.secret, function (err, decoded) {
-            if (err)
-                return res
-                    .status(500)
-                    .send({ auth: false, data: "Failed to authenticate token." });
-
-            conn
-                .connect()
-                .then(function () {
-                    var request = new sql.Request(conn);
-                    request.input("state", sql.VarChar(50), req.body.state);
-                    request.input("site", sql.VarChar(50), req.body.site);
-                    request.input("city", sql.VarChar(50), req.body.city);
-                    request.input("name", sql.VarChar(50), req.body.name);
-                    request.input("password", sql.VarChar(50), req.body.password);
-                    request.input("role", sql.VarChar(50), req.body.role);
-                    request.input("email", sql.VarChar(50), req.body.email);
-                    request.input("contact", sql.VarChar(50), req.body.contact);
-                    request.input("flag", sql.VarChar(10), "I");
-                    request.execute("sp_register", function (err, recordSet) {
-                        conn.close();
-                        if (err) {
-                            res.status(400).send(err);
-                        } else {
-                           // console.log(recordSet)
-                            if (recordSet.rowsAffected.length == 0) {
-                                res.status(200).send({ auth: "false", data: [] });
-                            } else {
-                                res
-                                    .status(200)
-                                    .send({ auth: "true", data: recordSet.rowsAffected[0] });
-                            }
-                        }
-                    });
-                })
-                .catch(function (err) {
-                    conn.close();
-                    res.status(400).send({ auth: "false", data: err });
-                });
+      conn
+        .connect()
+        .then(function() {
+          var request = new sql.Request(conn);
+          request.input("iniD", sql.VarChar(50), req.body.iniD);
+          request.input("sname", sql.VarChar(50), req.body.sname);
+          request.input("type", sql.VarChar(50), req.body.type);
+          request.input("size", sql.VarChar(50), req.body.size);
+          request.input("unit", sql.Int, req.body.unit);
+          request.input("siteid", sql.VarChar(50), req.body.siteid);
+          request.input("flag", sql.VarChar(10), "IN");
+          request.execute("sp_inoutitem", function(err, recordSet) {
+            conn.close();
+            if (err) {
+              res.status(400).send(err);
+            } else {
+              // console.log(recordSet)
+              if (recordSet.rowsAffected.length == 0) {
+                res.status(200).send({ auth: "false", data: [] });
+              } else {
+                res
+                  .status(200)
+                  .send({ auth: "true", data: recordSet.rowsAffected[0] });
+              }
+            }
+          });
+        })
+        .catch(function(err) {
+          conn.close();
+          res.status(400).send({ auth: "false", data: err });
         });
     });
+  });
 
-    router.route("/outitem").post(function (req, res) {
-        var token = req.headers["x-access-token"];
+  router.route("/outitem").post(function(req, res) {
+    var token = req.headers["x-access-token"];
 
-        if (!token)
-            return res
-                .status(401)
-                .send({ auth: false, message: "No token provided." });
+    if (!token)
+      return res
+        .status(401)
+        .send({ auth: false, message: "No token provided." });
 
-        jwt.verify(token, config.secret, function (err, decoded) {
-            if (err)
-                return res
-                    .status(500)
-                    .send({ auth: false, data: "Failed to authenticate token." });
+    jwt.verify(token, config.secret, function(err, decoded) {
+      if (err)
+        return res
+          .status(500)
+          .send({ auth: false, data: "Failed to authenticate token." });
 
-            conn
-                .connect()
-                .then(function () {
-                    var request = new sql.Request(conn);
-                    request.input("state", sql.VarChar(50), req.body.state);
-                    request.input("site", sql.VarChar(50), req.body.site);
-                    request.input("city", sql.VarChar(50), req.body.city);
-                    request.input("name", sql.VarChar(50), req.body.name);
-                    request.input("password", sql.VarChar(50), req.body.password);
-                    request.input("role", sql.VarChar(50), req.body.role);
-                    request.input("email", sql.VarChar(50), req.body.email);
-                    request.input("contact", sql.VarChar(50), req.body.contact);
-                    request.input("flag", sql.VarChar(10), "I");
-                    request.execute("sp_register", function (err, recordSet) {
-                        conn.close();
-                        if (err) {
-                            res.status(400).send(err);
-                        } else {
-                           // console.log(recordSet)
-                            if (recordSet.rowsAffected.length == 0) {
-                                res.status(200).send({ auth: "false", data: [] });
-                            } else {
-                                res
-                                    .status(200)
-                                    .send({ auth: "true", data: recordSet.rowsAffected[0] });
-                            }
-                        }
-                    });
-                })
-                .catch(function (err) {
-                    conn.close();
-                    res.status(400).send({ auth: "false", data: err });
-                });
+      conn
+        .connect()
+        .then(function() {
+          var request = new sql.Request(conn);          
+          request.input("id", sql.int, req.body.id);
+          request.input("runit", sql.Int, req.body.runit);
+          request.input("flag", sql.VarChar(10), "OUT");
+          request.execute("sp_inoutitem", function(err, recordSet) {
+            conn.close();
+            if (err) {
+              res.status(400).send(err);
+            } else {
+              // console.log(recordSet)
+              if (recordSet.rowsAffected.length == 0) {
+                res.status(200).send({ auth: "false", data: [] });
+              } else {
+                res
+                  .status(200)
+                  .send({ auth: "true", data: recordSet.rowsAffected[0] });
+              }
+            }
+          });
+        })
+        .catch(function(err) {
+          conn.close();
+          res.status(400).send({ auth: "false", data: err });
         });
     });
-    
-    return router;
-}
+  });
+
+  return router;
+};
 
 module.exports = InOutRoute;
